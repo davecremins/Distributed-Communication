cfa108292a025093cd81bf38a949c745daf000b9
Dave Cremins <dave.cremins@wpengine.com>
2017-01-26 23:41:40 +0000

    Creating simple view for messaging§

--- dweb_server/messages/views.py
+++ dweb_server/messages/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the messages index.")
+    template = loader.get_template('/messages/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
